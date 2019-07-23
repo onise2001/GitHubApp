95fbcb638b92f6358bf93fdf1759fe6bfda90687
georg31 <georg.digmelashvili@gmail.com>
2019-07-23 11:23:26 +0400

    s

---
+++ GitHubApplication/GitHubApplication/UserControls/UserInfoControl.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace GitHubApplication.UserControls
+{
+    partial class UserInfoControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FNameLabel = new System.Windows.Forms.Label();
+            this.LNameLabel = new System.Windows.Forms.Label();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.UserNameLabel = new System.Windows.Forms.Label();
+            this.PassLabel = new System.Windows.Forms.Label();
+            this.NewPassLabel = new System.Windows.Forms.Label();
+            this.RePasswordLabel = new System.Windows.Forms.Label();
+            this.FnameTextbox = new System.Windows.Forms.TextBox();
+            this.EmailTextbox = new System.Windows.Forms.TextBox();
+            this.LNameTextbox = new System.Windows.Forms.TextBox();
+            this.UserTextbox = new System.Windows.Forms.TextBox();
+            this.NewPassTextbox = new System.Windows.Forms.TextBox();
+            this.PassTextbox = new System.Windows.Forms.TextBox();
+            this.RePassTextbox = new System.Windows.Forms.TextBox();
+            this.ChangeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FNameLabel
+            // 
+            this.FNameLabel.AutoSize = true;
+            this.FNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FNameLabel.Location = new System.Drawing.Point(68, 17);
+            this.FNameLabel.Name = "FNameLabel";
+            this.FNameLabel.Size = new System.Drawing.Size(76, 17);
+            this.FNameLabel.TabIndex = 0;
+            this.FNameLabel.Text = "First Name";
+            // 
+            // LNameLabel
+            // 
+            this.LNameLabel.AutoSize = true;
+            this.LNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LNameLabel.Location = new System.Drawing.Point(68, 43);
+            this.LNameLabel.Name = "LNameLabel";
+            this.LNameLabel.Size = new System.Drawing.Size(76, 17);
+            this.LNameLabel.TabIndex = 0;
+            this.LNameLabel.Text = "Last Name";
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailLabel.Location = new System.Drawing.Point(97, 69);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(47, 17);
+            this.EmailLabel.TabIndex = 0;
+            this.EmailLabel.Text = "E-Mail";
+            // 
+            // UserNameLabel
+            // 
+            this.UserNameLabel.AutoSize = true;
+            this.UserNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNameLabel.Location = new System.Drawing.Point(65, 95);
+            this.UserNameLabel.Name = "UserNameLabel";
+            this.UserNameLabel.Size = new System.Drawing.Size(79, 17);
+            this.UserNameLabel.TabIndex = 0;
+            this.UserNameLabel.Text = "User Name";
+            // 
+            // PassLabel
+            // 
+            this.PassLabel.AutoSize = true;
+            this.PassLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PassLabel.Location = new System.Drawing.Point(75, 121);
+            this.PassLabel.Name = "PassLabel";
+            this.PassLabel.Size = new System.Drawing.Size(69, 17);
+            this.PassLabel.TabIndex = 0;
+            this.PassLabel.Text = "Password";
+            // 
+            // NewPassLabel
+            // 
+            this.NewPassLabel.AutoSize = true;
+            this.NewPassLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewPassLabel.Location = new System.Drawing.Point(44, 147);
+            this.NewPassLabel.Name = "NewPassLabel";
+            this.NewPassLabel.Size = new System.Drawing.Size(100, 17);
+            this.NewPassLabel.TabIndex = 0;
+            this.NewPassLabel.Text = "New Password";
+            // 
+            // RePasswordLabel
+            // 
+            this.RePasswordLabel.AutoSize = true;
+            this.RePasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RePasswordLabel.Location = new System.Drawing.Point(25, 173);
+            this.RePasswordLabel.Name = "RePasswordLabel";
+            this.RePasswordLabel.Size = new System.Drawing.Size(119, 17);
+            this.RePasswordLabel.TabIndex = 0;
+            this.RePasswordLabel.Text = "Repeat Password";
+            // 
+            // FnameTextbox
+            // 
+            this.FnameTextbox.Location = new System.Drawing.Point(150, 14);
+            this.FnameTextbox.Name = "FnameTextbox";
+            this.FnameTextbox.Size = new System.Drawing.Size(194, 20);
+            this.FnameTextbox.TabIndex = 1;
+            // 
+            // EmailTextbox
+            // 
+            this.EmailTextbox.Location = new System.Drawing.Point(150, 66);
+            this.EmailTextbox.Name = "EmailTextbox";
+            this.EmailTextbox.Size = new System.Drawing.Size(194, 20);
+            this.EmailTextbox.TabIndex = 1;
+            // 
+            // LNameTextbox
+            // 
+            this.LNameTextbox.Location = new System.Drawing.Point(150, 40);
+            this.LNameTextbox.Name = "LNameTextbox";
+            this.LNameTextbox.Size = new System.Drawing.Size(194, 20);
+            this.LNameTextbox.TabIndex = 1;
+            // 
+            // UserTextbox
+            // 
+            this.UserTextbox.Location = new System.Drawing.Point(150, 92);
+            this.UserTextbox.Name = "UserTextbox";
+            this.UserTextbox.Size = new System.Drawing.Size(194, 20);
+            this.UserTextbox.TabIndex = 1;
+            // 
+            // NewPassTextbox
+            // 
+            this.NewPassTextbox.Location = new System.Drawing.Point(150, 144);
+            this.NewPassTextbox.Name = "NewPassTextbox";
+            this.NewPassTextbox.Size = new System.Drawing.Size(194, 20);
+            this.NewPassTextbox.TabIndex = 1;
+            // 
+            // PassTextbox
+            // 
+            this.PassTextbox.Location = new System.Drawing.Point(150, 118);
+            this.PassTextbox.Name = "PassTextbox";
+            this.PassTextbox.Size = new System.Drawing.Size(194, 20);
+            this.PassTextbox.TabIndex = 1;
+            // 
+            // RePassTextbox
+            // 
+            this.RePassTextbox.Location = new System.Drawing.Point(150, 170);
+            this.RePassTextbox.Name = "RePassTextbox";
+            this.RePassTextbox.Size = new System.Drawing.Size(194, 20);
+            this.RePassTextbox.TabIndex = 1;
+            // 
+            // ChangeButton
+            // 
+            this.ChangeButton.Location = new System.Drawing.Point(150, 197);
+            this.ChangeButton.Name = "ChangeButton";
+            this.ChangeButton.Size = new System.Drawing.Size(194, 23);
+            this.ChangeButton.TabIndex = 2;
+            this.ChangeButton.Text = "Change Password";
+            this.ChangeButton.UseVisualStyleBackColor = true;
+            // 
+            // UserInfoControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ChangeButton);
+            this.Controls.Add(this.PassTextbox);
+            this.Controls.Add(this.LNameTextbox);
+            this.Controls.Add(this.RePassTextbox);
+            this.Controls.Add(this.NewPassTextbox);
+            this.Controls.Add(this.UserTextbox);
+            this.Controls.Add(this.EmailTextbox);
+            this.Controls.Add(this.FnameTextbox);
+            this.Controls.Add(this.RePasswordLabel);
+            this.Controls.Add(this.NewPassLabel);
+            this.Controls.Add(this.PassLabel);
+            this.Controls.Add(this.UserNameLabel);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.LNameLabel);
+            this.Controls.Add(this.FNameLabel);
+            this.Name = "UserInfoControl";
+            this.Size = new System.Drawing.Size(662, 279);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FNameLabel;
+        private System.Windows.Forms.Label LNameLabel;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.Label UserNameLabel;
+        private System.Windows.Forms.Label PassLabel;
+        private System.Windows.Forms.Label NewPassLabel;
+        private System.Windows.Forms.Label RePasswordLabel;
+        private System.Windows.Forms.TextBox FnameTextbox;
+        private System.Windows.Forms.TextBox EmailTextbox;
+        private System.Windows.Forms.TextBox LNameTextbox;
+        private System.Windows.Forms.TextBox UserTextbox;
+        private System.Windows.Forms.TextBox NewPassTextbox;
+        private System.Windows.Forms.TextBox PassTextbox;
+        private System.Windows.Forms.TextBox RePassTextbox;
+        private System.Windows.Forms.Button ChangeButton;
+    }
+}
